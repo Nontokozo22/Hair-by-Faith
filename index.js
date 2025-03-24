e64d8fc0c7089c9233b47d99808c8a941398ebfc
Nontokozo <nsdube10@gmail.com>
2025-03-24 14:17:25 +0200

    Updated the styling and index.js file

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-let hairstyles = document.querySelectorAll(`.adult-hairstyles`);
+
+
+let hairstyles = document.querySelectorAll(`.hairstyle`);
 
 hairstyles.forEach(hairstyle =>{
-    let averageTime = hairstyle.getAttribute(`data-average`)
-    let shortPrice =
-    hairstyle.getAttribute(`data-short-price`);
-    let longPrice = hairstyle.getAttribute('data-long-price');
+    let averageTime = hairstyle.getAttribute(`data-average-time`);
+    let shortPrice = hairstyle.getAttribute(`data-short-price`);
+    let longPrice = hairstyle.getAttribute(`data-long-price`);
 
     let hairstyleInfo = document.querySelectorAll(`div`);
+    jairstyleInfo.classList.add(`hairstyle-info`)
 
     hairstyleInfo.innerHTML = `<p> Average Time:${averageTime}</p>
     <p>Short:${shortPrice}</p>
@@ -14,3 +16,4 @@ hairstyles.forEach(hairstyle =>{
 
     hairstyle.appendChild(hairstyleInfo);
 })
+
